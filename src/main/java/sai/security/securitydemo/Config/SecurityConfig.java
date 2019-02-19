9c212defb55aa57fe781a323238ab65498a1dc6f
Sai Giridharan <Sai.Giridharan@jbhunt.com>
2019-02-19 06:25:57 +0000

    with inmemory user name and passwd

---
+++ src/main/java/sai/security/securitydemo/Config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package sai.security.securitydemo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("sai").password("2812").roles("USER").build());
+
+        return  new InMemoryUserDetailsManager(users);
+
+    }
+
+}
